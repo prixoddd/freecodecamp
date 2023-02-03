1b4a15b8f2207eda24883ef9a5ff0cd0dd3a46f5
Nikita <nikita.derjugin@gmail.com>
2023-02-03 21:57:30 +0200

    03.02. 5

--- 8.js
+++ 8.js
@@ -42,3 +42,14 @@ const myDog = {
   };
   
   myDog.bark = "woof";
+
+
+  function checkObj(obj, checkProp) {
+    // Only change code below this line
+    if (obj.hasOwnProperty(checkProp)) {
+        return obj[checkProp];
+      } else {
+        return "Not Found";
+      }
+    // Only change code above this line
+  }
