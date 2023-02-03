08e868848c7dee7597b19afe2c0430237d1ae0f5
Nikita <nikita.derjugin@gmail.com>
2023-02-03 22:08:40 +0200

    03.02. 6

--- 8.js
+++ 8.js
@@ -53,3 +53,17 @@ const myDog = {
       }
     // Only change code above this line
   }
+
+  const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  const gloveBoxContents = myStorage.car.inside["glove box"];
