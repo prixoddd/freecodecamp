3ebe2cc5f7e37f45be8afda153041980fd1f2134
Nikita <nikita.derjugin@gmail.com>
2023-02-04 13:23:14 +0200

    04.02 3

--- 12.js
+++ 12.js
@@ -51,3 +51,11 @@ const contacts = [
   
     // Only change code above this line
   }
+  
+  console.log(Math.floor(Math.random() * 20));
+
+  function randomRange(myMin, myMax) {
+    // Only change code below this line
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+    // Only change code above this line
+  }
