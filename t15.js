7e52d87c87c99d7c34e9189de2967aa20b6b1593
Nikita <70059012+prixoddd@users.noreply.github.com>
2023-02-21 21:54:27 +0200

    ger1

--- t15.js
+++ t15.js
@@ -7,3 +7,8 @@ console.log(A4.match(multipleA));
 let sampleWord = "astronaut";
 let pwRegex =  /(?=\w{6})(?=\w*\d{2})/;
 let result = pwRegex.test(sampleWord);
+
+let str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
+let replaceText = "$3 $2 $1"; // Change this line
+let result = str.replace(fixRegex, replaceText);
