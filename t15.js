e697727272c0f8a71f654c5f7a0294edb52c3ce4
Nikita <70059012+prixoddd@users.noreply.github.com>
2023-02-21 20:56:19 +0200

    ger

--- t15.js
+++ t15.js
@@ -4,3 +4,6 @@ let multipleA = /a{3,5}h/;
 console.log(A4.match(multipleA));
 // console.log(multipleA.match(A2));
 
+let sampleWord = "astronaut";
+let pwRegex =  /(?=\w{6})(?=\w*\d{2})/;
+let result = pwRegex.test(sampleWord);
