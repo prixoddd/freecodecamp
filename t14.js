5ec05121c288e53e6365e3dde5ea4d49e89a169c
Nikita <70059012+prixoddd@users.noreply.github.com>
2023-02-21 20:47:49 +0200

    ger

--- t14.js
+++ t14.js
@@ -8,8 +8,27 @@
 // let result = twinkleStar.match(starRegex); // Change this line
 // console.log(result);
 
-let humStr = "I'll hum a song";
-let hugStr = "Bear hug";
-let huRegex = /hu./;
-console.log(huRegex.test(humStr));
-console.log(huRegex.test(hugStr));
+// let humStr = "I'll hum a song";
+// let hugStr = "Bear hug";
+// let huRegex = /hu./;
+// console.log(huRegex.test(humStr));
+// console.log(huRegex.test(hugStr));
+
+
+// let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+// let vowelRegex = /[aeiou]/ig; // Change this line
+// let result = quoteSample.match(vowelRegex);
+// console.log(result);
+
+let phrase = "ba humbug";
+
+let regexPlus = /bah+/;
+let regexStar = /bah*/;
+
+console.log(phrase.match(regexPlus)); // returns false
+console.log(phrase.match(regexStar)); // returns true
+
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<.*?>/; // Change this line
+let result = text.match(myRegex);
+console.log(result);
---
+++ t15.js
@@ -0,0 +1,6 @@
+let A4 = "aaaah";
+let A2 = "aah";
+let multipleA = /a{3,5}h/;
+console.log(A4.match(multipleA));
+// console.log(multipleA.match(A2));
+
